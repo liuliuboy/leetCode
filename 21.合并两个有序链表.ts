e95635b8f7a389b9368bd0389e464b620eafadaf
liuliuboy <zg_ljz@163.com>
2023-01-02 19:24:00 +0800

    合并两个有序链表

---
+++ 21.合并两个有序链表.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=typescript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  let dummy = {
+    next: null
+  }
+  let temp:any = dummy;
+  while (list1 != null && list2 != null) {
+    if (list1.val <= list2.val) {
+      temp.next = list1;
+      list1 = list1.next;
+    } else {
+      temp.next = list2;
+      list2 = list2.next;
+    }
+    temp = temp.next
+  }
+  temp.next = list1 === null ? list2 : list1
+  return dummy.next
+};
+// @lc code=end
+
