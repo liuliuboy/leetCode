35f6fa82556fd2461dee1cd74fe1b4fe542ec8fe
liuliuboy <zg_ljz@163.com>
2023-01-09 14:18:26 +0800

    链表两数相加

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,6 +17,21 @@
  * }
  */
 function addTwoNumbers(l1, l2) {
-}
-;
+  let dummy = {
+    next: null
+  }
+  let curr = dummy;
+  let carry = 0;
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    let x = l1 ? l1.val : 0;
+    let y = l2 ? l2.val : 0;
+    let sum = x + y + carry;
+    carry = Math.floor(sum / 10); // 进位数 
+    curr.next = new ListNode(sum % 10); // 取余
+    curr = curr.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+  return dummy.next;
+};
 // @lc code=end
