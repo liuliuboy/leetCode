573db6a2e11116b8a0e20a0732c379ef7d215d73
liuliuboy <zg_ljz@163.com>
2023-01-10 10:47:32 +0800

    旋转链表

---
+++ 61.旋转链表.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=javascript
+ *
+ * [61] 旋转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+  if (k === 0) {
+    return head;
+  }
+  let dummy = {
+    next: head
+  }
+  let tmp = dummy;
+  let len = 1;
+  // 循环到最后一个节点
+  while (tmp.next !== null) {
+    tmp = tmp.next;
+    len++;
+  }
+  // 最后一个节点指向头结点形成环
+  tmp.next = head;
+  k = len - k % len;
+};
+// @lc code=end
+
