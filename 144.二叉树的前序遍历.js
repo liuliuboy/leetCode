8bdf7c73a5b9d04ba9c024f55754cb1f77f1355b
liuliuboy <zg_ljz@163.com>
2023-01-22 13:07:06 +0800

    二叉树前序遍历

---
+++ 144.二叉树的前序遍历.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+ 
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+   // 递归
+   let res = [];
+   let dfs = (cur) => {
+       res.push(cur.val);
+       cur.left && dfs(cur.left);
+       cur.right && dfs(cur.right);
+   }
+   root && dfs(root);
+   return res;
+
+  // 迭代
+  // let res = [];
+  // if (root === null) {
+  //   return res;
+  // }
+  // let stack = [root]
+  // while (stack.length) {
+  //   let cur = stack.pop();
+  //   res.push(cur.val);
+  //   cur.right && stack.push(cur.right);
+  //   cur.left && stack.push(cur.left);
+  // }
+  // return res;
+};
+// @lc code=end
+
