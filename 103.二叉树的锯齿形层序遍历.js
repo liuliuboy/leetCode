90cf8f34457eb2987d57267ea1e75f0b620a971f
liuliuboy <zg_ljz@163.com>
2023-02-03 10:37:14 +0800

    树

--- 103.二叉树的锯齿形层序遍历.js
+++ 103.二叉树的锯齿形层序遍历.js
@@ -22,23 +22,25 @@ var zigzagLevelOrder = function(root) {
   if (root === null) {
     return []
   }
-  let ret = [];
-  let stk = [root];
+  let ret = []; // 结果
+  let stk = [root]; // 队列
 
   let isOrderLeft = true;
   while (stk.length) {
-    let len = stk.length;
+    let len = stk.length; // 队列
     let leveList = [];
+    // 循环队列
     for (let i = 0; i < len; i++) {
-      let node = stk.shift();
+      let node = stk.shift(); // 先进先出
       if (isOrderLeft) {
         leveList.push(node.val);
       } else {
         leveList.unshift(node.val);
       }
-
+      
+      // 判断当前节点是否有左右子节点，有的话放入队列中
       if (node.left !== null) {
-        stk.push(node.left);
+        stk.push(node.left); 
       }
       if (node.right !== null) {
         stk.push(node.right);
