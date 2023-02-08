4f624a4938d1e9f9ac3a7de3f615923270e13bdd
liuliuboy <zg_ljz@163.com>
2023-02-08 16:00:58 +0800

    迭代树的最大深度

--- 104.二叉树的最大深度.js
+++ 104.二叉树的最大深度.js
@@ -16,12 +16,39 @@
 /**
  * @param {TreeNode} root
  * @return {number}
+ * 递归
+ */
+// var maxDepth = function(root) {
+//   if (root === null) {
+//     return 0;
+//   }
+//   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+// };
+
+/**
+ * 迭代
+ * 广度优先遍历
  */
 var maxDepth = function(root) {
   if (root === null) {
-    return 0;
+    return 0
   }
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-};
+  let deep = 0;
+  let stk = [root];
+  while(stk.length) {
+    let len = stk.length;
+    for (let i = 0; i < len; i++) {
+      let node = stk.shift();
+      if (node.left !== null) {
+        stk.push(node.left);
+      }
+      if (node.right !== null) {
+        stk.push(node.right);
+      }
+    }
+    deep++;
+  }
+  return deep;
+}
 // @lc code=end
 
