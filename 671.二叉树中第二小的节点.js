ccc56b272dbff4ecffc7e4b8c21b4679a8659051
liuliuboy <zg_ljz@163.com>
2023-02-09 22:41:47 +0800

    找到第二小节点

---
+++ 671.二叉树中第二小的节点.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=671 lang=javascript
+ *
+ * [671] 二叉树中第二小的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function(root) {
+  let stk = [root];
+  let res = [];
+  // 遍历树
+  while (stk.length) {
+    let node = stk.pop();
+    res.push(node.val);
+    node.left && stk.push(node.left);
+    node.right && stk.push(node.right);
+  }
+  // 去重+排序
+  let arr = [...new Set(res)].sort();
+  return arr[1] ? arr[1] : -1;
+};
+// @lc code=end
+
