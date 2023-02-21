2d888f5f9654e4dfd8f265d1428c7bd79fbd8d66
liuliuboy <zg_ljz@163.com>
2023-02-21 17:01:34 +0800

    N 叉树的最大深度

--- 559.n-叉树的最大深度.js
+++ 559.n-叉树的最大深度.js
@@ -21,11 +21,26 @@ var maxDepth = function(root) {
   if (root === null) {
     return 0
   }
-  let dep = 1;
+  let dep = 0;
   let stk = [root];
-  while () {
-
+  // 
+  while (stk.length) {
+    // 获取队列长度
+    let len = stk.length;
+    while (len > 0) {
+      // 广度优先
+      const node = stk.shift();
+      node.children && node.children.map((item) => {
+        // 获取当前节点子节点，放入队列中
+        stk.push(item);
+      })
+      // 更新队列长度
+      len--;
+    }
+    // 计算层级
+    dep++;
   }
+  return dep;
 };
 // @lc code=end
 
