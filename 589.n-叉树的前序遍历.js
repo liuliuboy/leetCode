b4140e7ca3790ebbbfa235dfa35565934c9f1963
liuliuboy <zg_ljz@163.com>
2023-02-28 10:39:55 +0800

    N 叉树前序遍历

--- 589.n-叉树的前序遍历.js
+++ 589.n-叉树的前序遍历.js
@@ -18,7 +18,22 @@
  * @return {number[]}
  */
 var preorder = function(root) {
-    
+  if (root === null) {
+    return [];
+  }
+  let stk = [root];
+  let ret = [];
+  while (stk.length) {
+    // 栈
+    let node = stk.pop();
+    ret.push(node.val);
+    // 倒序 后进先出 原则
+    let i = node.children ? node.children.length - 1 : 0;
+    for (i; i >= 0; i--) {
+      stk.push(node.children[i]);
+    }
+  }
+  return ret;
 };
 // @lc code=end
 
