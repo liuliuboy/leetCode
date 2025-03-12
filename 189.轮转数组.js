70d2d6915d7da3b27a14ebaaff102d159b34940f
liujinze666 <liuliuliucn@gmail.com>
2025-03-12 13:45:34 +0800

    取余

---
+++ 189.轮转数组.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k %= nums.length; // 取余数
+    nums.unshift(...nums.splice(-k, k));
+};
+// @lc code=end
+
