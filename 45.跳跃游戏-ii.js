5af3b430255cac4f1577001e1d012a4742b15cc0
liujinze666 <liuliuliucn@gmail.com>
2025-03-24 14:05:56 +0800

    贪心算法

--- 45.跳跃游戏-ii.js
+++ 45.跳跃游戏-ii.js
@@ -10,16 +10,33 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let step = 0;
-    let end = 0;
-    let maxPosition = 0;
-    for (let i = 0; i < nums.length - 1; i++) {
-        maxPosition = Math.max(maxPosition, i + nums[i]);
-        if (i === end) {
-            step++;
-            end = maxPosition;
+    // let step = 0;
+    // let end = 0;
+    // let maxPosition = 0;
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     maxPosition = Math.max(maxPosition, i + nums[i]);
+    //     if (i === end) {
+    //         step++;
+    //         end = maxPosition;
+    //     }
+    // }
+
+
+    // 第二种方式实现 贪心算法
+    let len = nums.length - 1;
+    let steps = 0;
+    while (len > 0) {
+        let max = 0;
+        let index = 0;
+        for (let i = 0; i < len; i++) {
+            if (i + nums[i] > max) {
+                max = i + nums[i];
+                index = i;
+                break;
+            }
         }
-    }    
+        steps++;
+    }
     return step;
 };
 // @lc code=end
